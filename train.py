9a044564d8be6fc084f9f1f431437b724c4b5273
Struan Robertson <struanrobertson@protonmail.com>
2023-04-09 00:23:41 +0100

    Resume from saved models

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from trainer import Trainer
 from dataset import Dataset
 from tools import random_bbox, mask_image, apply_colormap, make_grid
 
-with open('config.toml', 'r') as file:
+with open("config.toml", "r") as file:
     config = toml.load(file)
 
 ##### Initialise
@@ -44,30 +44,31 @@ train_loader = DataLoader(
 )
 
 trainer = Trainer(config)
-#print(trainer.netG)
-#print(trainer.localD)
-#print(trainer.globalD)
+# print(trainer.netG)
+# print(trainer.localD)
+# print(trainer.globalD)
 
 if cuda:
     trainer = trainer.cuda()
 
 # Get the resume iteration to restart training
-start_iteration = trainer.resume(config["resume"]) if config["resume"] else 1
+start_epoch = (
+    trainer.resume(config["checkpoint_save_path"], config["resume"])
+    if config["resume"]
+    else 1
+)
 
 iterable_train_loader = iter(train_loader)
 
 time_count = time.time()
 
-for epoch in range (config["epochs"]):
-
+for epoch in range(config["epochs"]):
     # For correctly saving snapshots
-    epoch += 1
+    epoch += 1 + start_epoch
 
     # TODO maybe use iteration instead of epoch for saving snapshots and visualisations, as with v large dataset takes a long time for each epoch
 
     for iteration, ground_truth in enumerate(train_loader):
-
-
         # Prepare inputs
         bboxes = random_bbox(config, batch_size=ground_truth.size(0))
         x, mask = mask_image(ground_truth, bboxes, config)
@@ -79,21 +80,25 @@ for epoch in range (config["epochs"]):
         #### Forward pass
         # Only compute generator loss after 'n_critic' iterations, usually 5 as defined in the Wasserstein GAN paper
         compute_g_loss = iteration % config["n_critic"] == 0
-        losses, inpainted_result = trainer(x, bboxes, mask, ground_truth, compute_g_loss)
+        losses, inpainted_result = trainer(
+            x, bboxes, mask, ground_truth, compute_g_loss
+        )
 
         #### Backward Pass
         # Update D
         trainer.optimizer_d.zero_grad()
-        losses['d'] = losses['wgan_d'] + losses['wgan_gp'] * config['wgan_gp_lambda']
-        losses['d'].backward()
+        losses["d"] = losses["wgan_d"] + losses["wgan_gp"] * config["wgan_gp_lambda"]
+        losses["d"].backward()
 
         # Update G
         if compute_g_loss:
             trainer.optimizer_g.zero_grad()
-            losses['g'] = losses['l1'] * config['l1_loss_alpha'] \
-                        + losses['ae'] * config['ae_loss_alpha'] \
-                        + losses['wgan_g'] * config['gan_loss_alpha']
-            losses['g'].backward()
+            losses["g"] = (
+                losses["l1"] * config["l1_loss_alpha"]
+                + losses["ae"] * config["ae_loss_alpha"]
+                + losses["wgan_g"] * config["gan_loss_alpha"]
+            )
+            losses["g"].backward()
             trainer.optimizer_g.step()
 
         # Has to come afterwards
@@ -102,42 +107,60 @@ for epoch in range (config["epochs"]):
         # TODO Epoch size doesnt change and can just be printed at the start
         # TODO Print start and end time
 
-        log_losses = ['l1', 'ae', 'wgan_g', 'wgan_d', 'wgan_gp', 'g', 'd']
-        if epoch % config['print_iter'] == 0 and iteration == 0:
+        log_losses = ["l1", "ae", "wgan_g", "wgan_d", "wgan_gp", "g", "d"]
+        if epoch % config["print_iter"] == 0 and iteration == 0:
             time_count = time.time() - time_count
-            speed = config['print_iter'] / time_count
-            speed_msg = f'speed: {speed * 60} epochs/min'
+            speed = config["print_iter"] / time_count
+            speed_msg = f"speed: {speed * 60} epochs/min"
             time_count = time.time()
 
-            #message = 'Iter: %d/%d, ' % (iteration, config['epochs'])
-            message = f'Epoch: {epoch}, Epoch Size: {train_loader.__len__()}, '
+            # message = 'Iter: %d/%d, ' % (iteration, config['epochs'])
+            message = f"Epoch: {epoch}, Epoch Size: {train_loader.__len__()}, "
 
             for k in log_losses:
-                v = losses.get(k, 0.)
-                message += '%s: %.6f, ' % (k, v)
+                v = losses.get(k, 0.0)
+                message += "%s: %.6f, " % (k, v)
 
             message += speed_msg
             print(message)
 
-        if epoch % config['snapshot_save_iter'] == 0 and iteration == 0:
-            trainer.save_model(os.path.join(config["checkpoint_save_path"], "saved_models"), epoch)
+        if epoch % config["snapshot_save_iter"] == 0 and iteration == 0:
+            trainer.save_model(
+                os.path.join(config["checkpoint_save_path"], "saved_models"), epoch
+            )
 
-        if epoch % (config['viz_iter']) == 0 and iteration == 0:
+        if epoch % (config["viz_iter"]) == 0 and iteration == 0:
+            viz_max_out = config["viz_max_out"]
 
-                viz_max_out = config['viz_max_out']
+            # if x.size(0) > viz_max_out:
+            #     viz_images = torch.stack([x[:viz_max_out,0], inpainted_result[:viz_max_out,0]], dim=1)
+            # else:
+            #     viz_images = torch.stack([x[:,0], inpainted_result[:,0]], dim=1)
 
-                # if x.size(0) > viz_max_out:
-                #     viz_images = torch.stack([x[:viz_max_out,0], inpainted_result[:viz_max_out,0]], dim=1)
-                # else:
-                #     viz_images = torch.stack([x[:,0], inpainted_result[:,0]], dim=1)
+            if x.size(0) > viz_max_out:
+                viz_dem = torch.cat(
+                    (
+                        x[:viz_max_out, 0].data,
+                        inpainted_result[:viz_max_out, 0].data,
+                        ground_truth[:viz_max_out, 0].data,
+                    ),
+                    -2,
+                )
+            else:
+                viz_dem = torch.cat(
+                    (
+                        x[:, 0].data,
+                        inpainted_result[:, 0].data,
+                        ground_truth[:, 0].data,
+                    ),
+                    -2,
+                )
 
-                if x.size(0) > viz_max_out:
-                    viz_dem = torch.cat((x[:viz_max_out, 0].data, inpainted_result[:viz_max_out, 0].data, ground_truth[:viz_max_out, 0].data), -2)
-                else:
-                    viz_dem = torch.cat((x[:,0].data, inpainted_result[:,0].data, ground_truth[:,0].data), -2)
+            viz_dem = apply_colormap(viz_dem, "terrain")
 
-                viz_dem = apply_colormap(viz_dem, 'terrain')
+            dem_grid = make_grid(viz_dem)
 
-                dem_grid = make_grid(viz_dem)
-
-                plt.imsave(os.path.join(config["checkpoint_save_path"], f'images/{epoch}_dem.png'), dem_grid)
+            plt.imsave(
+                os.path.join(config["checkpoint_save_path"], f"images/{epoch}_dem.png"),
+                dem_grid,
+            )
--- trainer.py
+++ trainer.py
@@ -6,21 +6,28 @@ from torch import autograd
 from networks import Generator, LocalDis, GlobalDis
 from tools import get_model_list, local_patch, spatial_discounting_mask
 
+
 class Trainer(nn.Module):
     def __init__(self, config):
         super(Trainer, self).__init__()
         self.config = config
-        self.use_cuda = self.config['cuda']
+        self.use_cuda = self.config["cuda"]
 
         self.netG = Generator(self.config, self.use_cuda)
         self.localD = LocalDis(self.config, self.use_cuda)
         self.globalD = GlobalDis(self.config, self.use_cuda)
 
-        self.optimizer_g = torch.optim.Adam(self.netG.parameters(), lr=self.config['lr'],
-                                            betas=(self.config['beta1'], self.config['beta2']))
+        self.optimizer_g = torch.optim.Adam(
+            self.netG.parameters(),
+            lr=self.config["lr"],
+            betas=(self.config["beta1"], self.config["beta2"]),
+        )
         d_params = list(self.localD.parameters()) + list(self.globalD.parameters())
-        self.optimizer_d = torch.optim.Adam(d_params, lr=config['lr'],
-                                            betas=(self.config['beta1'], self.config['beta2']))
+        self.optimizer_d = torch.optim.Adam(
+            d_params,
+            lr=config["lr"],
+            betas=(self.config["beta1"], self.config["beta2"]),
+        )
         if self.use_cuda:
             self.netG = self.netG.cuda()
             self.localD = self.localD.cuda()
@@ -33,42 +40,58 @@ class Trainer(nn.Module):
 
         x1, x2 = self.netG(x, masks)
         local_patch_gt = local_patch(ground_truth, bboxes)
-        x1_inpaint = x1 * masks + x * (1. - masks)
-        x2_inpaint = x2 * masks + x * (1. - masks)
+        x1_inpaint = x1 * masks + x * (1.0 - masks)
+        x2_inpaint = x2 * masks + x * (1.0 - masks)
         local_patch_x1_inpaint = local_patch(x1_inpaint, bboxes)
         local_patch_x2_inpaint = local_patch(x2_inpaint, bboxes)
 
         # D part
         # wgan d loss
         local_patch_real_pred, local_patch_fake_pred = self.dis_forward(
-            self.localD, local_patch_gt, local_patch_x2_inpaint.detach())
+            self.localD, local_patch_gt, local_patch_x2_inpaint.detach()
+        )
         global_real_pred, global_fake_pred = self.dis_forward(
-            self.globalD, ground_truth, x2_inpaint.detach())
-        losses['wgan_d'] = torch.mean(local_patch_fake_pred - local_patch_real_pred) + \
-            torch.mean(global_fake_pred - global_real_pred) * self.config['global_wgan_loss_alpha']
+            self.globalD, ground_truth, x2_inpaint.detach()
+        )
+        losses["wgan_d"] = (
+            torch.mean(local_patch_fake_pred - local_patch_real_pred)
+            + torch.mean(global_fake_pred - global_real_pred)
+            * self.config["global_wgan_loss_alpha"]
+        )
         # gradients penalty loss
         local_penalty = self.calc_gradient_penalty(
-            self.localD, local_patch_gt, local_patch_x2_inpaint.detach())
-        global_penalty = self.calc_gradient_penalty(self.globalD, ground_truth, x2_inpaint.detach())
-        losses['wgan_gp'] = local_penalty + global_penalty
+            self.localD, local_patch_gt, local_patch_x2_inpaint.detach()
+        )
+        global_penalty = self.calc_gradient_penalty(
+            self.globalD, ground_truth, x2_inpaint.detach()
+        )
+        losses["wgan_gp"] = local_penalty + global_penalty
 
         # G part
         if compute_loss_g:
             sd_mask = spatial_discounting_mask(self.config)
-            losses['l1'] = l1_loss(local_patch_x1_inpaint * sd_mask, local_patch_gt * sd_mask) * \
-                self.config['coarse_l1_alpha'] + \
-                l1_loss(local_patch_x2_inpaint * sd_mask, local_patch_gt * sd_mask)
-            losses['ae'] = l1_loss(x1 * (1. - masks), ground_truth * (1. - masks)) * \
-                self.config['coarse_l1_alpha'] + \
-                l1_loss(x2 * (1. - masks), ground_truth * (1. - masks))
+            losses["l1"] = l1_loss(
+                local_patch_x1_inpaint * sd_mask, local_patch_gt * sd_mask
+            ) * self.config["coarse_l1_alpha"] + l1_loss(
+                local_patch_x2_inpaint * sd_mask, local_patch_gt * sd_mask
+            )
+            losses["ae"] = l1_loss(
+                x1 * (1.0 - masks), ground_truth * (1.0 - masks)
+            ) * self.config["coarse_l1_alpha"] + l1_loss(
+                x2 * (1.0 - masks), ground_truth * (1.0 - masks)
+            )
 
             # wgan g loss
             local_patch_real_pred, local_patch_fake_pred = self.dis_forward(
-                self.localD, local_patch_gt, local_patch_x2_inpaint)
+                self.localD, local_patch_gt, local_patch_x2_inpaint
+            )
             global_real_pred, global_fake_pred = self.dis_forward(
-                self.globalD, ground_truth, x2_inpaint)
-            losses['wgan_g'] = - torch.mean(local_patch_fake_pred) - \
-                torch.mean(global_fake_pred) * self.config['global_wgan_loss_alpha']
+                self.globalD, ground_truth, x2_inpaint
+            )
+            losses["wgan_g"] = (
+                -torch.mean(local_patch_fake_pred)
+                - torch.mean(global_fake_pred) * self.config["global_wgan_loss_alpha"]
+            )
 
         return losses, x2_inpaint
 
@@ -98,9 +121,14 @@ class Trainer(nn.Module):
         if self.use_cuda:
             grad_outputs = grad_outputs.cuda()
 
-        gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                                  grad_outputs=grad_outputs, create_graph=True,
-                                  retain_graph=True, only_inputs=True)[0]
+        gradients = autograd.grad(
+            outputs=disc_interpolates,
+            inputs=interpolates,
+            grad_outputs=grad_outputs,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
 
         gradients = gradients.view(batch_size, -1)
         gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
@@ -111,38 +139,52 @@ class Trainer(nn.Module):
         self.eval()
         x1, x2, offset_flow = self.netG(x, masks)
         # x1_inpaint = x1 * masks + x * (1. - masks)
-        x2_inpaint = x2 * masks + x * (1. - masks)
+        x2_inpaint = x2 * masks + x * (1.0 - masks)
 
         return x2_inpaint, offset_flow
 
     def save_model(self, checkpoint_dir, iteration):
         # Save generators, discriminators, and optimizers
-        gen_name = os.path.join(checkpoint_dir, 'gen_%08d.pt' % iteration)
-        dis_name = os.path.join(checkpoint_dir, 'dis_%08d.pt' % iteration)
-        opt_name = os.path.join(checkpoint_dir, 'optimizer.pt')
+        gen_name = os.path.join(checkpoint_dir, "gen_%08d.pt" % iteration)
+        dis_name = os.path.join(checkpoint_dir, "dis_%08d.pt" % iteration)
+        opt_name = os.path.join(checkpoint_dir, "optimizer.pt")
         torch.save(self.netG.state_dict(), gen_name)
-        torch.save({'localD': self.localD.state_dict(),
-                    'globalD': self.globalD.state_dict()}, dis_name)
-        torch.save({'gen': self.optimizer_g.state_dict(),
-                    'dis': self.optimizer_d.state_dict()}, opt_name)
+        torch.save(
+            {"localD": self.localD.state_dict(), "globalD": self.globalD.state_dict()},
+            dis_name,
+        )
+        torch.save(
+            {
+                "gen": self.optimizer_g.state_dict(),
+                "dis": self.optimizer_d.state_dict(),
+            },
+            opt_name,
+        )
 
-    def resume(self, checkpoint_dir, iteration=0, test=False):
-        # Load generators
-        last_model_name = get_model_list(checkpoint_dir, "gen", iteration=iteration)
-        self.netG.load_state_dict(torch.load(last_model_name))
-        iteration = int(last_model_name[-11:-3])
+    def resume(self, checkpoint_dir, epoch=1):
+        checkpoint_dir = os.path.join(checkpoint_dir, "saved_models")
 
-        if not test:
-            # Load discriminators
-            last_model_name = get_model_list(checkpoint_dir, "dis", iteration=iteration)
-            state_dict = torch.load(last_model_name)
-            self.localD.load_state_dict(state_dict['localD'])
-            self.globalD.load_state_dict(state_dict['globalD'])
-            # Load optimizers
-            state_dict = torch.load(os.path.join(checkpoint_dir, 'optimizer.pt'))
-            self.optimizer_d.load_state_dict(state_dict['dis'])
-            self.optimizer_g.load_state_dict(state_dict['gen'])
+        # Get file names from epoch
+        epoch_string = "00000000"
+        epoch_string = epoch_string[: 8 - len(str(epoch))] + str(epoch) + ".pt"
 
-        print("Resume from {} at iteration {}".format(checkpoint_dir, iteration))
+        gen_string = os.path.join(checkpoint_dir, "gen_" + epoch_string)
+        dis_string = os.path.join(checkpoint_dir, "dis_" + epoch_string)
+        opt_string = os.path.join(checkpoint_dir, "optimizer.pt")
 
-        return iteration
+        # Generator
+        self.netG.load_state_dict(torch.load(gen_string))
+
+        # Discriminator
+        state_dict = torch.load(dis_string)
+        self.localD.load_state_dict(state_dict["localD"])
+        self.globalD.load_state_dict(state_dict["globalD"])
+
+        # Optimizers
+        state_dict = torch.load(opt_string)
+        self.optimizer_d.load_state_dict(state_dict["dis"])
+        self.optimizer_g.load_state_dict(state_dict["gen"])
+
+        print("Resume from {} at epoch {}".format(checkpoint_dir, epoch))
+
+        return epoch
