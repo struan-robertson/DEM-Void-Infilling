41c7ec4bae79eff8ea4f1b8627a701b31816e13e
Struan Robertson <struanrobertson@protonmail.com>
2023-04-25 00:27:57 +0100

    Conv comments

--- networks.py
+++ networks.py
@@ -34,27 +34,30 @@ class CoarseGenerator(nn.Module):
         super(CoarseGenerator, self).__init__()
         self.use_cuda = use_cuda
 
-        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
-        self.conv2_downsample = gen_conv(cnum, cnum*2, 3, 2, 1)
-        self.conv3 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
-        self.conv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1)
-        self.conv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2) #4 x 256 x256
+        self.conv2_downsample = gen_conv(cnum, cnum*2, 3, 2, 1) # 32 x 256 x 256
 
-        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2)
-        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4)
-        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8)
-        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16)
+        self.conv3 = gen_conv(cnum*2, cnum*2, 3, 1, 1) # 64 x 128 x 128
+        self.conv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1) # 64 x 128 x 128
 
-        self.conv11 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.conv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
 
-        self.conv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1,)
-        self.conv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
-        self.conv15 = gen_conv(cnum*2, cnum, 3, 1, 1, )
-        self.conv16 = gen_conv(cnum, cnum//2, 3, 1, 1, )
-        self.conv17 = gen_conv(cnum//2, input_dim, 3, 1, 1)
-        self.conv18 = gen_conv(input_dim, input_dim, 5, 1, 2, activation='tanh')
+        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2) # 128 x 64 x 64
+        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4) # 128 x 64 x 64
+        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8) # 128 x 64 x 64
+        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16) # 128 x 64 x 64
+
+        self.conv11 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+        self.conv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+        # Resize
+        self.conv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1,) # 128 x 128 x 128
+        self.conv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1) # 64 x 128 x 128
+        # Resize
+        self.conv15 = gen_conv(cnum*2, cnum, 3, 1, 1, ) # 64 x 256 x 256
+        self.conv16 = gen_conv(cnum, cnum//2, 3, 1, 1, ) # 32 x 256 x 256
+        self.conv17 = gen_conv(cnum//2, input_dim, 3, 1, 1) # 16 x 256 x 256
+        self.conv18 = gen_conv(input_dim, input_dim, 5, 1, 2, activation='tanh') # 2 x 256 x 256
 
     def forward(self, x, mask):
 
@@ -98,43 +101,44 @@ class FineGenerator(nn.Module):
         super(FineGenerator, self).__init__()
         self.use_cuda = use_cuda
 
-        # 3 x 256 x 256
-        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
-        self.conv2_downsample = gen_conv(cnum, cnum, 3, 2, 1)
-        # cnum*2 x 128 x 128
-        self.conv3 = gen_conv(cnum, cnum*2, 3, 1, 1)
-        self.conv4_downsample = gen_conv(cnum*2, cnum*2, 3, 2, 1)
-        # cnum*4 x 64 x 64
-        self.conv5 = gen_conv(cnum*2, cnum*4, 3, 1, 1)
-        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
+        self.conv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2) # 4 x 256 x 256
+        self.conv2_downsample = gen_conv(cnum, cnum, 3, 2, 1) # 32 x 256 x 256
 
-        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2)
-        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4)
-        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8)
-        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16)
+        self.conv3 = gen_conv(cnum, cnum*2, 3, 1, 1) # 32 x 128 x 128
+        self.conv4_downsample = gen_conv(cnum*2, cnum*2, 3, 2, 1) # 64 x 128 x 128
+
+        self.conv5 = gen_conv(cnum*2, cnum*4, 3, 1, 1) # 64 x 64 x 64
+        self.conv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+
+        self.conv7_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 2, rate=2) # 128 x 64 x 64
+        self.conv8_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 4, rate=4) # 128 x 64 x 64
+        self.conv9_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 8, rate=8) # 128 x 64 x 64
+        self.conv10_atrous = gen_conv(cnum*4, cnum*4, 3, 1, 16, rate=16) # 128 x 64 x 64
 
         # attention branch
-        # 3 x 256 x 256
-        self.pmconv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2)
-        self.pmconv2_downsample = gen_conv(cnum, cnum, 3, 2, 1)
-        # cnum*2 x 128 x 128
-        self.pmconv3 = gen_conv(cnum, cnum*2, 3, 1, 1)
-        self.pmconv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1)
-        # cnum*4 x 64 x 64
-        self.pmconv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.pmconv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1, activation='relu')
+        self.pmconv1 = gen_conv(input_dim + 2, cnum, 5, 1, 2) #4 x 256 x 256
+        self.pmconv2_downsample = gen_conv(cnum, cnum, 3, 2, 1) #32 x 256 x 256
+
+        self.pmconv3 = gen_conv(cnum, cnum*2, 3, 1, 1) # 32 x 128 x 128
+        self.pmconv4_downsample = gen_conv(cnum*2, cnum*4, 3, 2, 1) # 64 x 128 x 128
+
+        self.pmconv5 = gen_conv(cnum*4, cnum*4, 3, 1, 1)  # 128 x 64 x 64
+        self.pmconv6 = gen_conv(cnum*4, cnum*4, 3, 1, 1, activation='relu') # 128 x 64 x 64
         self.contextul_attention = ContextualAttention(ksize=3, stride=1, rate=2, fuse_k=3, softmax_scale=10,
                                                        fuse=True, use_cuda=self.use_cuda)
-        self.pmconv9 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.pmconv10 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.allconv11 = gen_conv(cnum*8, cnum*4, 3, 1, 1)
-        self.allconv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1)
-        self.allconv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1)
-        self.allconv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1)
-        self.allconv15 = gen_conv(cnum*2, cnum, 3, 1, 1)
-        self.allconv16 = gen_conv(cnum, cnum//2, 3, 1, 1)
-        self.allconv17 = gen_conv(cnum//2, input_dim, 3, 1, 1)
-        self.allconv18 = gen_conv(input_dim, input_dim, 7, 1, 3, activation='tanh')
+        self.pmconv9 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+        self.pmconv10 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+
+        self.allconv11 = gen_conv(cnum*8, cnum*4, 3, 1, 1) # 256 x 64 x 64
+        self.allconv12 = gen_conv(cnum*4, cnum*4, 3, 1, 1) # 128 x 64 x 64
+        # Resize
+        self.allconv13 = gen_conv(cnum*4, cnum*2, 3, 1, 1) # 128 x 128 x 128
+        self.allconv14 = gen_conv(cnum*2, cnum*2, 3, 1, 1) # 64 x 128 x 128
+        # Resize
+        self.allconv15 = gen_conv(cnum*2, cnum, 3, 1, 1) # 64 x 256 x 256
+        self.allconv16 = gen_conv(cnum, cnum//2, 3, 1, 1) # 32 x 256 x 256
+        self.allconv17 = gen_conv(cnum//2, input_dim, 3, 1, 1) # 16 x 256 x 256
+        self.allconv18 = gen_conv(input_dim, input_dim, 7, 1, 3, activation='tanh') # 2 x 256 x 256
 
     def forward(self, xin, x_stage1, mask):
         x1_inpaint = x_stage1 * mask + xin * (1. - mask)
@@ -366,10 +370,10 @@ class DisConvModule(nn.Module):
         super(DisConvModule, self).__init__()
         self.use_cuda = use_cuda
 
-        self.conv1 = dis_conv(input_dim, cnum, 4, 2, 1, norm='none')
-        self.conv2 = dis_conv(cnum, cnum*2, 4, 2, 1)
-        self.conv3 = dis_conv(cnum*2, cnum*4, 4, 2, 1)
-        self.conv4 = dis_conv(cnum*4, cnum*4, 4, 2, 1)
+        self.conv1 = dis_conv(input_dim, cnum, 4, 2, 1, norm='none') # 2 x 256 x 256
+        self.conv2 = dis_conv(cnum, cnum*2, 4, 2, 1) # 64 x 128 x 128
+        self.conv3 = dis_conv(cnum*2, cnum*4, 4, 2, 1) # 128 x 64 x 64
+        self.conv4 = dis_conv(cnum*4, cnum*4, 4, 2, 1) # 256 x 32 x 32
 
     def forward(self, x):
 
